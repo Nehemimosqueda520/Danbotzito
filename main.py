cebe411e0e82e21030dd72eec7a5d63703bdaa2c
nehemimosqueda520 <nehemimosqueda8@gmail.com>
2025-11-14 16:44:42 -0300

    env example

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = discord.Client(intents=intents)  
 
 @client.event
 async def on_ready():
